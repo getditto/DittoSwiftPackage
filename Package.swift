1fd9fd164a1f297c4f8ab6b24517108b69d47439
Ben Chatelain <ben@ditto.live>
2022-01-24 16:49:08 -0700

    🔀 Point binary download to new repo

--- Package.swift
+++ Package.swift
@@ -16,11 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DittoSwift",
-
-            // url: "https://software.ditto.live/cocoa/DittoSwift/1.0.19/dist/DittoSwift-dist.tar.gz",
-            // checksum: 57a49ac2b63c8bb4d7e7cfc70145fa7f91d75649a8b5235fef5e0f858f1adbd3
-
-            url: "https://github.com/phatblat/Ditto.swift/releases/download/1.0.19/DittoSwift.xcframework.zip",
+            url: "https://github.com/getditto/DittoSwift/releases/download/1.0.19/DittoSwift.xcframework.zip",
             checksum: "93c65ae87fdd43eb1282bffc33047f543ba8e00d49fbf97bd4c5118be4d5d76b"
         )
     ]
