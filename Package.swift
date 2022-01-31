c62b16977ff4096a6e613138d5e7fff42090c563
Ben Chatelain <ben@ditto.live>
2022-01-30 21:26:25 -0700

    ⏲ Use local xcframework, remove DittoObjC dependency

--- Package.swift
+++ Package.swift
@@ -10,14 +10,16 @@ let package = Package(
             name: "DittoSwift",
             targets: ["DittoSwift"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/getditto/DittoObjC", .exact("1.1.0")),
-    ],
+//    dependencies: [
+//        .package(url: "https://github.com/getditto/DittoObjC", .exact("1.1.0")),
+//    ],
     targets: [
         .binaryTarget(
             name: "DittoSwift",
-            url: "https://github.com/getditto/DittoSwift/releases/download/1.1.0/DittoSwift.xcframework.zip",
-            checksum: "18ea89247e58cc0b279929a8ad472dac306b96c007d81ed25b6d1b5bc75f4c23"
+            path: "DittoSwift.xcframework"
+
+//            url: "https://github.com/getditto/DittoSwift/releases/download/1.1.0/DittoSwift.xcframework.zip",
+//            checksum: "18ea89247e58cc0b279929a8ad472dac306b96c007d81ed25b6d1b5bc75f4c23"
         )
     ]
 )
