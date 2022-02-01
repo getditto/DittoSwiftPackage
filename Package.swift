546801b9c78742a84f9d3656cb77dfd9aa3a092c
Ben Chatelain <ben@ditto.live>
2022-02-01 16:54:26 -0700

    ♻️ Replace DittoObjC dependency with library and binaryTarget

--- Package.swift
+++ Package.swift
@@ -9,15 +9,20 @@ let package = Package(
         .library(
             name: "DittoSwift",
             targets: ["DittoSwift"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/getditto/DittoObjC", .exact("1.0.19")),
+        .library(
+            name: "DittoObjC",
+            targets: ["DittoObjC"]),
     ],
     targets: [
         .binaryTarget(
             name: "DittoSwift",
             url: "https://github.com/getditto/DittoSwift/releases/download/1.0.19/DittoSwift.xcframework.zip",
             checksum: "93c65ae87fdd43eb1282bffc33047f543ba8e00d49fbf97bd4c5118be4d5d76b"
-        )
+        ),
+        .binaryTarget(
+            name: "DittoObjC",
+            url: "https://github.com/getditto/DittoObjC/releases/download/1.0.19/DittoObjC.xcframework.zip",
+            checksum: "08526a7e68415e2499674d171fd929e6c8cf3571581a8e7d7619d735e7612770"
+        ),
     ]
 )
