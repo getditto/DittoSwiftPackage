7ea15c95255fbc45ff6f15d231abdb95f33296ae
Ben Chatelain <phatblat@users.noreply.github.com>
2022-02-11 17:05:41 -0700

    🔗 Update URL to DittoObjC archive

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "DittoObjC",
-            url: "https://github.com/getditto/DittoObjC/releases/download/1.1.0/DittoObjC.xcframework.zip",
-            checksum: "f4e1ea50cc21986f7cb23c2be50519d61cea43b99d50ddff794b4a164dd4cd27"
+            url: "https://github.com/getditto/DittoObjC/releases/download/1.1.1/DittoObjC.xcframework.zip",
+            checksum: "8a4f656e05ab45b143b371e472f335d945fa6c03528d1904b575a36ca36114ee"
         ),
     ]
 )
