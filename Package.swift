960cddc07902955c99661a7ffcedbe97dd45c1db
Ben Chatelain <ben@ditto.live>
2022-02-13 16:37:23 -0700

    🚑 Fix artifact download URLs

--- Package.swift
+++ Package.swift
@@ -16,12 +16,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DittoSwift",
-            url: "https://github.com/getditto/DittoSwift/releases/download/1.1.1/DittoSwift.xcframework.zip",
-            checksum: "9bbf1d367c62eb39d6bb9c5b48f7bffd2c906573ed065a562cff2df5ec3ec81c"
+            url: "https://github.com/getditto/DittoSwiftPackage/releases/download/1.1.1/DittoSwift.xcframework.zip",
+            checksum: "3923c02bdcfcabb436007c6eaee01590a9d73ceed7d2bb1569be28ea96baabc0"
         ),
         .binaryTarget(
             name: "DittoObjC",
-            url: "https://github.com/getditto/DittoObjC/releases/download/1.1.1/DittoObjC.xcframework.zip",
+            url: "https://github.com/getditto/DittoSwiftPackage/releases/download/1.1.1/DittoObjC.xcframework.zip",
             checksum: "8a4f656e05ab45b143b371e472f335d945fa6c03528d1904b575a36ca36114ee"
         ),
     ]
