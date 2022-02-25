ceadc116d7e69c0163ebbcb44a685953a11a6ce0
Ben Chatelain <ben@ditto.live>
2022-02-25 08:42:57 -0700

    🔖 Set checksums for released 1.1.2 binaries

--- Package.swift
+++ Package.swift
@@ -17,12 +17,12 @@ let package = Package(
         .binaryTarget(
             name: "DittoSwift",
             url: "https://github.com/getditto/DittoSwiftPackage/releases/download/1.1.2/DittoSwift.xcframework.zip",
-            checksum: "?"
+            checksum: "ab35eb04286be704a9a115216035de5a43f6a8f57037390b848566e842e27279"
         ),
         .binaryTarget(
             name: "DittoObjC",
             url: "https://github.com/getditto/DittoSwiftPackage/releases/download/1.1.2/DittoObjC.xcframework.zip",
-            checksum: "?"
+            checksum: "1af01b8bae8411cafab6758adb09decd17cfe89179f4f3d6e6908a75b2ad319e"
         ),
     ]
 )
