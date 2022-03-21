c001c09e937f75dc0751f21d63d2b014e029aad4
Ben Chatelain <phatblat@users.noreply.github.com>
2022-03-21 12:21:19 -0600

    🔢 Add artifact hashes

--- Package.swift
+++ Package.swift
@@ -17,12 +17,12 @@ let package = Package(
         .binaryTarget(
             name: "DittoSwift",
             url: "https://github.com/getditto/DittoSwiftPackage/releases/download/1.1.3/DittoSwift.xcframework.zip",
-            checksum: "?"
+            checksum: "dd99fe30b7ff984a879e50c6f57a0a0f49448541562c015b2f6e48841705687c"
         ),
         .binaryTarget(
             name: "DittoObjC",
             url: "https://github.com/getditto/DittoSwiftPackage/releases/download/1.1.3/DittoObjC.xcframework.zip",
-            checksum: "?"
+            checksum: "cc200ccfd103bd8893ba23d800d9c9c39347b69e5bd021aa233e31345d2a7afa"
         ),
     ]
 )
