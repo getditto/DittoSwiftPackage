2acfa8c6ec89332574d8d04b14907e1749dd0726
Ben Chatelain <phatblat@users.noreply.github.com>
2022-05-06 10:44:54 -0600

    🔗 Update URLs and hashes for 1.1.6 artifacts

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DittoSwift",
-            url: "https://github.com/getditto/DittoSwiftPackage/releases/download/1.1.5/DittoSwift.xcframework.zip",
-            checksum: "c965b43da578113baeeaa7f67f068112f40a0ac635a2c06bee44d26277166c07"
+            url: "https://github.com/getditto/DittoSwiftPackage/releases/download/1.1.6/DittoSwift.xcframework.zip",
+            checksum: "fa53bbf6482dc4a04ab9d30876c27a3da252807ed656c936b0db037d7ba4cae8"
         ),
         .binaryTarget(
             name: "DittoObjC",
-            url: "https://github.com/getditto/DittoSwiftPackage/releases/download/1.1.5/DittoObjC.xcframework.zip",
-            checksum: "f631fe84166dc83be8742b4d1c51d2ea03f153a1acfdb54a6f36ef00f5b2eb42"
+            url: "https://github.com/getditto/DittoSwiftPackage/releases/download/1.1.6/DittoObjC.xcframework.zip",
+            checksum: "ddee0f43bcbc2719dbcd70101e3cfcd87d82a9aa0bc0eeb6c74e2e7f59950084"
         ),
     ]
 )
