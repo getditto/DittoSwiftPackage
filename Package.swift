43d2351acff9b336b16b84c7e88f552d7027e596
Ditto Tester <tester@ditto.live>
2023-04-28 11:43:02 -0700

    Release ckage-publish-4.1

--- Package.swift
+++ Package.swift
@@ -23,13 +23,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "DittoSwift",
-            url: "https://software.ditto.live/cocoa/DittoSwift/4.0.2/dist/DittoSwift.xcframework.zip",
-            checksum: "0cafeec19d94724a89f22d8316d42ee47c5728f99a46f2b283da03e8ee38eb7f"
+            url: "https://software.ditto.live/cocoa/DittoSwift/ckage-publish-4.1/dist/DittoSwift.xcframework.zip",
+            checksum: "2a3053c28b7f4392e4d216c511407672f4863f36a056a71fea47a5db33b17877"
         ),
         .binaryTarget(
             name: "DittoObjC",
-            url: "https://software.ditto.live/cocoa/DittoObjC/4.0.2/dist/DittoObjC.xcframework.zip",
-            checksum: "edf933e4aff9be9ed2a16f555e2a3cef2bc2942a3b4d2499a550812f5f9db06d"
+            url: "https://software.ditto.live/cocoa/DittoObjC/ckage-publish-4.1/dist/DittoObjC.xcframework.zip",
+            checksum: "97c6017afdd037a3f14655a3705ef7531e6d4d18a65aabc0438da7c67c1ca76e"
         ),
     ]
 )
