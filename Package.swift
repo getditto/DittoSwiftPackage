0f507956ada1d0aa998f940f408138b1bbdcf822
Ben Chatelain <ben@octop.ad>
2024-04-10 16:44:43 -0600

    ⚗️ Point binaryTargets at local xcframework paths

--- Package.swift
+++ Package.swift
@@ -27,13 +27,11 @@ let package = Package(
         ),
         .binaryTarget(
             name: "DittoSwift",
-            url: "https://software.ditto.live/cocoa/DittoSwift/4.7.1/dist/DittoSwift.xcframework.zip",
-            checksum: "37ee1b0cc02ecbe00cdeba3d788b7b37e025ae9217429423232ae47f4ef3413e"
+            path: "DittoSwift.xcframework"
         ),
         .binaryTarget(
             name: "DittoObjC",
-            url: "https://software.ditto.live/cocoa/DittoObjC/4.7.1/dist/DittoObjC.xcframework.zip",
-            checksum: "895152e94097debf1f27fcd16cfcd850eba481196a1904adcdf72b292a2361a1"
+            path: "DittoObjC.xcframework"
         ),
     ]
 )
