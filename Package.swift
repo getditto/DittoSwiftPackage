da18044257b87803d98925ff6dfef6e4932d526d
Ditto CI <tester@ditto.live>
2024-05-30 12:37:28 -0700

    Release 4.8.0-catalyst-alpha3

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Ditto",
-    platforms: [ .iOS(.v11), .macOS(.v11) ],
+    platforms: [ .iOS(.v14), .macOS(.v11), .macCatalyst(.v14) ],
     products: [
         .library(
             name: "DittoSwift",
@@ -23,13 +23,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "DittoSwift",
-            url: "https://software.ditto.live/cocoa/DittoSwift/4.7.2-rc.1/dist/DittoSwift.xcframework.zip",
-            checksum: "f8437989e508cd07b7226f9821940ecb7a47eb623f027979da3c25b973562bef"
+            url: "https://software.ditto.live/cocoa/DittoSwift/4.8.0-catalyst-alpha3/dist/DittoSwift.xcframework.zip",
+            checksum: "369d819cafd8eab3be485b32c7b66ba3e91bd2b99bb97e8b96465e705684cfad"
         ),
         .binaryTarget(
             name: "DittoObjC",
-            url: "https://software.ditto.live/cocoa/DittoObjC/4.7.2-rc.1/dist/DittoObjC.xcframework.zip",
-            checksum: "613d1ba836148fcf72e57c81cb973bc8af3596c6fc5ffc4fceb358f979925949"
+            url: "https://software.ditto.live/cocoa/DittoObjC/4.8.0-catalyst-alpha3/dist/DittoObjC.xcframework.zip",
+            checksum: "2b5ed323b194a3032091c1674e3f0c5ef882d3e5f69f8c404ed20bca457bb205"
         ),
     ]
 )
