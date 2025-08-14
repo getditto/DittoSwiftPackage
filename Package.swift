dbd7ece5f263ff65a0c0dea083a634defe5e1f5e
Konstantin Bender <me@konstantinbender.com>
2025-08-14 21:25:03 +0200

    Remove DittoObjC

--- Package.swift
+++ Package.swift
@@ -8,28 +8,13 @@ let package = Package(
     products: [
         .library(
             name: "DittoSwift",
-            targets: ["DittoSwiftWrapper"]),
-        .library(
-            name: "DittoObjC",
-            targets: ["DittoObjC"]),
+            targets: ["DittoSwift"]),
     ],
     targets: [
-        .target(
-            name: "DittoSwiftWrapper",
-            dependencies: [
-                .target(name: "DittoSwift"),
-                .target(name: "DittoObjC"),
-            ]
-        ),
         .binaryTarget(
             name: "DittoSwift",
             url: "https://software.ditto.live/cocoa/DittoSwift/5.0.0-experimental.kb-inline-objc-into-swift-sdk.3/dist/DittoSwift.xcframework.zip",
             checksum: "309ef212435b9314e9aa55287190cc88896fc3bd0b8b70ce9378b5e0e754d0f2"
-        ),
-        .binaryTarget(
-            name: "DittoObjC",
-            url: "DittoObjC.xcframework.zip.url",
-            checksum: "DittoObjC.xcframework.zip.checksum"
         ),
     ]
 )
