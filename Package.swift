e642aec542dd4d02e299105aba87474d42a94097
Ditto CI <tester@ditto.live>
2025-08-14 03:05:23 -0600

    Release 5.0.0-experimental.kb-inline-objc-into-swift-sdk.1

--- Package.swift
+++ Package.swift
@@ -23,13 +23,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "DittoSwift",
-            url: "https://software.ditto.live/cocoa/DittoSwift/4.11.3-rc.1/dist/DittoSwift.xcframework.zip",
-            checksum: "f5c2d8f22c86354e98a763989dcf9c3bd23bb578bd8e283a0f45504d75ba26cf"
+            url: "https://software.ditto.live/cocoa/DittoSwift/5.0.0-experimental.kb-inline-objc-into-swift-sdk.1/dist/DittoSwift.xcframework.zip",
+            checksum: "7ab469c20aface32e06fee1a4fa1c01428ccfca7568b4e5b3cdfb61dff8088a7"
         ),
         .binaryTarget(
             name: "DittoObjC",
-            url: "https://software.ditto.live/cocoa/DittoObjC/4.11.3-rc.1/dist/DittoObjC.xcframework.zip",
-            checksum: "a95b9f7a7d4fd06a5ec97a24ebb4c0ea499ff02b81403d51904902a5d64ea1eb"
+            url: "DittoObjC.xcframework.zip.url",
+            checksum: "DittoObjC.xcframework.zip.checksum"
         ),
     ]
 )
