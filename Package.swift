1b0fab7b3dbe2a34e0487ea21825e2354b4f79cd
Ditto CI <tester@ditto.live>
2025-09-10 12:01:11 -0400

    Release 4.12.2-rc.1

--- Package.swift
+++ Package.swift
@@ -23,13 +23,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "DittoSwift",
-            url: "https://software.ditto.live/cocoa/DittoSwift/4.11.5-rc.1/dist/DittoSwift.xcframework.zip",
-            checksum: "7c55059d29ab13206c3c5621163830668bcf97628bd97ff957071caea335abc9"
+            url: "https://software.ditto.live/cocoa/DittoSwift/4.12.2-rc.1/dist/DittoSwift.xcframework.zip",
+            checksum: "d65becaf68a020a87a957f993d239d2f23fd337e22a133492a92d2d039c9e12d"
         ),
         .binaryTarget(
             name: "DittoObjC",
-            url: "https://software.ditto.live/cocoa/DittoObjC/4.11.5-rc.1/dist/DittoObjC.xcframework.zip",
-            checksum: "d42d8b66b10c17bdb3a5510d18d05aabf0e26e288d2226b16ed041bf19154402"
+            url: "https://software.ditto.live/cocoa/DittoObjC/4.12.2-rc.1/dist/DittoObjC.xcframework.zip",
+            checksum: "fd2648a3e530cdef106604d5b78716f1313fa33317b8597ed3f1129bb9b3f30e"
         ),
     ]
 )
