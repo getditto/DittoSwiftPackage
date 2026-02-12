7a0e9ddd0147825a2fb40984d43589451e856bfd
Ditto CI <tester@ditto.live>
2026-02-12 15:03:14 -0500

    Release 5.1.0-experimental-all-sdks.1

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DittoSwift",
-            url: "https://software.ditto.live/cocoa/DittoSwift/5.0.0-preview.4/dist/DittoSwift.xcframework.zip",
-            checksum: "f00fa1d9e9b2822bdba0feac6984a3a429ae8ae0f91e89699b0089853dab55b2"
+            url: "https://software.ditto.live/cocoa/DittoSwift/5.1.0-experimental-all-sdks.1/dist/DittoSwift.xcframework.zip",
+            checksum: "7f1babf006100669b44093a66e9c2cd55f80769668738c665dd86e886f1e8f5c"
         )
     ]
 )
