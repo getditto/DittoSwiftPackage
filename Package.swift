b86dab00ad7d6c93188b10ce228ad465c5757b29
Ditto CI <tester@ditto.live>
2026-05-19 00:16:36 +0000

    Release 4.11.8-experimental-gha-workflow.2

--- Package.swift
+++ Package.swift
@@ -23,13 +23,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "DittoSwift",
-            url: "https://software.ditto.live/cocoa/DittoSwift/4.11.8-experimental-gha-workflow.1/dist/DittoSwift.xcframework.zip",
-            checksum: "67b6d3d7ba2fd9206430d04891d46824af9a359bf1512ce7f2c2799f1ff23b45"
+            url: "https://software.ditto.live/cocoa/DittoSwift/4.11.8-experimental-gha-workflow.2/dist/DittoSwift.xcframework.zip",
+            checksum: "d1bacfdd18e03aed84b1d3e8200ee3c5fd0f4a8ac8d72e5336b9161056bc72f2"
         ),
         .binaryTarget(
             name: "DittoObjC",
-            url: "https://software.ditto.live/cocoa/DittoObjC/4.11.8-experimental-gha-workflow.1/dist/DittoObjC.xcframework.zip",
-            checksum: "d36bbbda1868c742a8f871f1395548ab97f7bf57db27e50fcba5860c2c52125c"
+            url: "https://software.ditto.live/cocoa/DittoObjC/4.11.8-experimental-gha-workflow.2/dist/DittoObjC.xcframework.zip",
+            checksum: "8b3a7c8ce8ca76b2a37eb0b3cb96a27ee15e46cb10eafe0820c5cca4784025c8"
         ),
     ]
 )
