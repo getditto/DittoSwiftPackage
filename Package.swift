85291a0a4f128e60a8fbcc6ed40a2d162503806d
Ditto CI <tester@ditto.live>
2026-09-08 18:45:37 +0000

    Release 5.0.4-rc.1

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Ditto",
-    platforms: [ .iOS(.v15), .macOS(.v12), .macCatalyst(.v15) ],
+    platforms: [ .iOS(.v15), .macOS(.v12), .macCatalyst(.v15), .tvOS(.v15) ],
     products: [
         .library(
             name: "DittoSwift",
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DittoSwift",
-            url: "https://software.ditto.live/swift/DittoSwift/5.2.0-dev-weekly.20260906.2376/dist/DittoSwift.xcframework.zip",
-            checksum: "8f28d2688b00db15dd27eb7fb3a417a280ded073bac5bd309310897e169d3849"
+            url: "https://software.ditto.live/cocoa/DittoSwift/5.0.4-rc.1/dist/DittoSwift.xcframework.zip",
+            checksum: "53339bef03607fff9e968bacfd00435e5b677f8728bb7788512cc9074933a8c4"
         )
     ]
 )
